4961c68a7d0c9f0e7712a5b1eae0d25d6e24ddad
Christopher DeVito <christopher.devito@protonmail.com>
2022-01-19 14:51:53 -0500

    Finished NBAFinals1

--- NBAFinals.playground/Pages/NBAFinals1.xcplaygroundpage/Contents.swift
+++ NBAFinals.playground/Pages/NBAFinals1.xcplaygroundpage/Contents.swift
@@ -2,13 +2,43 @@
 
 import Foundation
 
-let bundle = Bundle.main
+let NBAYears = getCSVAsDict()
 
-guard let path = bundle.path(forResource: "nba_finals", ofType: "csv") else { fatalError("path is nil") }
+struct NBAFinalsYear {
+    let year: Int
+    let winner: String
+    let loser: String
+    let score: String
+    let MVP: String
+}
 
-guard let content = try? String(contentsOfFile: path) else { fatalError("content2020 is nil") }
+func getCSVAsDict() -> [Int: NBAFinalsYear] {
+    let bundle = Bundle.main
+    guard let path = bundle.path(forResource: "nba_finals", ofType: "csv") else { fatalError("path is nil") }
+    guard let content = try? String(contentsOfFile: path) else { fatalError("content is nil") }
+    var NBAYears = content.components(separatedBy: .newlines)
+    NBAYears.removeFirst()
 
-var babyNames = content.components(separatedBy: .newlines)
-babyNames = babyNames.map { $0.uppercased() }
-babyNames.popLast()
+    var NBAYearsDict: [Int: NBAFinalsYear] = [:]
+
+    for NBAYear in NBAYears {
+        if NBAYear == "" {
+            continue
+        }
+
+        let yearComponents = NBAYear.components(separatedBy: ",")
+        guard let year = Int(yearComponents[0]) else { continue }
+        let yearEntry = NBAFinalsYear(year: year, winner: yearComponents[1], loser: yearComponents[2], score: yearComponents[3], MVP: yearComponents[4])
+        NBAYearsDict[year] = yearEntry
+    }
+
+    return NBAYearsDict
+}
+
+func winner(forYear year: Int) -> String {
+    NBAYears[year]?.winner ?? "That year is not available in this data."
+}
+
+print(winner(forYear: 1999))
+
 //: [Next](@next)
